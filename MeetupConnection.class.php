d4f434d6cfb1dce60f392a2a68a77957b08f9a19
Chris LaRose <cjlarose@gmail.com>
2012-07-15 18:31:19 +0000

    MeetupConnection is now an abstract class with two implementing classes, one for each authentication method

--- MeetupConnection.class.php
+++ MeetupConnection.class.php
@@ -4,26 +4,134 @@
  * Currently only supports api key authentication, but can be extended to
  * support oAuth.
  */
-class MeetupConnection {
-    public $allowed_auth_types = array('key');
-    public $auth_type;
-    public $auth_params;
-    /*
-     * Initializes a connection to the Meetup API
+abstract class MeetupConnection {
+	abstract protected function get( $Url );
+	abstract protected function buildUrl( $Endpoint, $Parameters, $RequiredParameters );
+    /**
+     * Checks the input parameters against a list of required parameters to
+     * ensure at least one of the required parameters exists.
+     *
+     * NOTE: The Meetup API contains a list of parameters that are required for
+     * each endpoint with a default condition of "any of"
      * 
-     * @param String $auth_type - The authentication type. Only 'key' is supported
+     * @param Array $RequiredList - Names of required parameters
+     * @param Array $Parameters - List of provided paramters
+     * @return Boolean
      */
-    public function __construct($auth_type, $params=array()) {
-        if (in_array($auth_type, $this->allowed_auth_types)) {
-            $this->auth_type = $auth_type;
-            if ($auth_type == 'key') {
-                if (array_key_exists('key', $params))
-                    $this->auth_params = $params;
-                else
-                    throw new Exception('Invalid parameters for MeetupConnection construction');
+    public function verifyParameters($RequiredList, $Parameters) {
+        $Parameters = array_keys($Parameters);
+
+        /*
+         * Check to see if any of the required list is in the parameters array
+         * Since the Meetup API requires "any of" if a required key is found in
+         * parameters the verification will pass
+         */
+        foreach($RequiredList AS $r) {
+            if(in_array($r, $Parameters)) {
+                return true;
             }
-        } else {
-            throw new Exception('Invalid parameters for MeetupConnection construction');
         }
+        return false;
+    }
+}
+
+class KeyAuthMeetupConnection extends MeetupConnection {
+	/*
+	* Initializes a connection to the Meetup API
+	* 
+	* @param String $auth_type - The authentication type. Only 'key' is supported
+	*/
+	private $_key;
+
+	public function __construct($key) {
+		$this->_key = $key;
+	}
+
+    /**
+     * Performs the GET query against the specified endpoint
+     *
+     * @throws MeetupBadRequestException
+     * @throws MeetupUnauthorizedRequestException
+     * @throws MeetupInternalServerErrorException
+     * @param String $Url - Endpoint with URL paramters (for now)
+     * @return MeetupApiResponse
+     */
+    public function get( $Url ) {
+        // Clear error status
+        $ret = false;
+        
+        $options = array(
+            CURLOPT_RETURNTRANSFER => true,     // return web page
+            CURLOPT_HEADER         => true,    // don't return headers
+            CURLOPT_USERAGENT      => "PHP Meetup.com API Client", // who am i
+            CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+            CURLOPT_TIMEOUT        => 120,      // timeout on response
+            CURLOPT_HEADER => 0
+        );
+//echo $Url;
+        $ch = curl_init( $Url );
+        curl_setopt_array( $ch, $options );
+        $content = curl_exec( $ch );
+        $header  = curl_getinfo( $ch );
+        curl_close( $ch );
+        $header['content'] = $content;
+
+        $response = new MeetupApiResponse();
+        $response->setHttpCode($header['http_code']);
+        $response->setResponse($header['content']);
+        
+
+
+        if( $response->getHttpCode() == '400' ) {
+            // 400 Bad request when there was a problem with the request
+            throw new MeetupBadRequestException($Url, $response);
+        } else if ( $response->getHttpCode() == '401' ) {
+            // 401 Unauthorized when you don't provide a valid key
+            throw new MeetupUnauthorizedRequestException();
+        } else if ( $response->getHttpCode() == '500' ) {
+            // 500 Internal Server Error
+            throw new MeetupInternalServerErrorException();
+        }
+
+        return $response;
+    }
+
+    /**
+     *
+     * @throws MeetupInvalidParametersException
+     * @param <type> $Endpoint
+     * @param <type> $Parameters
+     * @param <type> $RequiredParameters - Optional - Some endpoints do not have parameters
+     * @return <type>
+     */
+    public function buildUrl( $Endpoint, $Parameters, $RequiredParameters = null ) {
+        if(is_array($RequiredParameters) && !$this->verifyParameters( $RequiredParameters, $Parameters )) {
+            throw new MeetupInvalidParametersException( $RequiredParameters );
+        }
+        $params = '';
+        foreach($Parameters AS $k => $v) {
+            $params .= "&$k=$v";
+        }
+        return MEETUP_API_URL . $Endpoint . "?key=" . $this->_key . $params;
+    }
+
+}
+
+class OAuth2MeetupConnection extends MeetupConnection {
+	public function __construct($client_id, $redirect_uri) {
+		
+	}	
+	public function get($url) {
+
+	}
+    public function buildUrl( $Endpoint, $Parameters, $RequiredParameters = null ) {
+        if(is_array($RequiredParameters) && !$this->verifyParameters( $RequiredParameters, $Parameters )) {
+            throw new MeetupInvalidParametersException( $RequiredParameters );
+        }
+        $params = '';
+        foreach($Parameters AS $k => $v) {
+            $params .= "&$k=$v";
+        }
+        return MEETUP_API_URL . $Endpoint . "?key=" . $this->_conn->auth_params['key'] . $params;
     }
 }
