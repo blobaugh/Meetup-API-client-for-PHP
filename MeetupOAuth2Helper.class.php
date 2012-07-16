8d2544c57ac3605c0e18f014f595bc409d6dee83
Chris LaRose <cjlarose@gmail.com>
2012-07-16 04:51:54 +0000

    Added support for refreshing access tokens in MeetupOAuth2Helper
    * Static oauth helper class returns the full response object for request_access_token
    * Added private _access_request method in static oauth helper class to generalize requests for new access tokens
    * Added refresh_access_token method to static oauth helper class
    * Replaced tab characters with spaces as per convention
    * Updated documentation on MeetupOAuth2Helper class

--- MeetupOAuth2Helper.class.php
+++ MeetupOAuth2Helper.class.php
@@ -27,49 +27,22 @@ class MeetupOAuth2Helper {
     }
 
     /**
-     * Request an API access token
+     * Private method for POST requests against the meetup access token
+     * endpoint.  Used for requesting API access tokens.
      *
      * @throws MeetupInvalidParametersException
      * @throws MeetupBadRequestException
      * @throws MeetupUnauthorizedRequestException
      * @throws MeetupInternalServerErrorException
      * 
-     * @param Array $param - An asociative array containing at least the following
-     * key-value pairs:
-     * 
-     * client_id - Your App's key obtained from 
-     * http://www.meetup.com/meetup_api/oauth_consumers/
-     * client_secret - Your App's secret obtained from 
-     * http://www.meetup.com/meetup_api/oauth_consumers/
-     * redirect_uri - The URI pointing to where you want to 
-     * redirect your users after a successful authorization request
-     * code - The code received from an authorization response
-     * The $options array may also contain a 'user_agent' to be used for the cURL
-     * request
-     * 
-     * @return String the access token upon a successful request
-     * @return Boolean returns false upon unsuccessful request
+     * @param Array $fields - The POST parameters
+     * @param String $user_agent - the user agent for the cURL request
+     * @return object|boolean An object containing attributes access_token, expires_in,
+     * and refresh_token upon a successful request; returns false upon 
+     * unsuccessful request
      */
-    public static function request_access_token($params) { 
-
-        $required_params = array('client_id', 'client_secret', 'redirect_uri', 'code');
-        foreach ($required_params as $param_key) {
-            if ( !array_key_exists($param_key, $params) ) {
-                throw new MeetupInvalidParametersException($required_params);
-                return FALSE;
-            }
-        }
-
+    private static function _access_request($fields, $user_agent=NULL) {
         $url = MEETUP_ACCESS_TOKEN_URL;
-
-        # Set up POST parameters to access token endpoint
-        $fields = array(
-            "code" => $params['code'],
-            "client_id" => $params['client_id'],
-            "client_secret" => $params['client_secret'],
-            "redirect_uri" => $params['redirect_uri'],
-            "grant_type" => "authorization_code"
-        );
 
         # Set the cURL options
         $curl_options = array(
@@ -82,8 +55,8 @@ class MeetupOAuth2Helper {
             CURLOPT_POSTFIELDS     => http_build_query($fields)
         );
 
-        if ( array_key_exists('user_agent', $params) ) {
-            $curl_options[CURLOPT_USERAGENT] = $params['user_agent'];
+        if ( $user_agent != NULL ) {
+            $curl_options[CURLOPT_USERAGENT] = $user_agent;
         }
 
         # Perform the cURL request
@@ -97,8 +70,7 @@ class MeetupOAuth2Helper {
         $response_obj = json_decode($content);
 
         if ( $status_code == "200" ) {
-            $access_token = $response_obj->access_token;
-            return $access_token;
+            return $response_obj;
         } else {
             switch( $status_code ) {
                 case "400":
@@ -114,5 +86,111 @@ class MeetupOAuth2Helper {
         }
 
         return FALSE;
+
+    }
+
+    /**
+     * Request an API access token
+     *
+     * @throws MeetupInvalidParametersException
+     * @throws MeetupBadRequestException
+     * @throws MeetupUnauthorizedRequestException
+     * @throws MeetupInternalServerErrorException
+     * 
+     * @param Array $param - An asociative array containing at least the 
+     * following key-value pairs:
+     * 
+     * client_id - Your App's key obtained from 
+     * http://www.meetup.com/meetup_api/oauth_consumers/
+     * client_secret - Your App's secret obtained from 
+     * http://www.meetup.com/meetup_api/oauth_consumers/
+     * redirect_uri - The URI pointing to where you want to 
+     * redirect your users after a successful authorization request
+     * code - The code received from an authorization response
+     * The $options array may also contain a 'user_agent' to be used for the 
+     * cURL request
+     * 
+     * @return object|boolean An object containing attributes access_token, expires_in,
+     * and refresh_token upon a successful request; returns false upon 
+     * unsuccessful request
+     */
+    public static function request_access_token($params) { 
+
+        $required_params = array('client_id', 'client_secret', 'redirect_uri', 'code');
+        foreach ( $required_params as $param_key ) {
+            if ( !array_key_exists($param_key, $params) ) {
+                throw new MeetupInvalidParametersException($required_params);
+                return FALSE;
+            }
+        }
+
+        # Set up POST parameters to access token endpoint
+        $fields = array(
+            "code" => $params['code'],
+            "client_id" => $params['client_id'],
+            "client_secret" => $params['client_secret'],
+            "redirect_uri" => $params['redirect_uri'],
+            "grant_type" => "authorization_code"
+        );
+
+        $user_agent = NULL; 
+        if ( array_key_exists('user_agent', $params) ) {
+            $user_agent = $params['user_agent'];
+        }
+        
+        $access_response = MeetupOAuth2Helper::_access_request($fields, $user_agent);
+        return $access_response;
+
+    }
+
+    /**
+     * Refresh an API access token
+     *
+     * @throws MeetupInvalidParametersException
+     * @throws MeetupBadRequestException
+     * @throws MeetupUnauthorizedRequestException
+     * @throws MeetupInternalServerErrorException
+     * 
+     * @param Array $param - An asociative array containing at least the 
+     * following key-value pairs:
+     * 
+     * client_id - Your App's key obtained from 
+     * http://www.meetup.com/meetup_api/oauth_consumers/
+     * client_secret - Your App's secret obtained from 
+     * http://www.meetup.com/meetup_api/oauth_consumers/
+     * refresh_token - The refresh token received from an access token response
+     * The $options array may also contain a 'user_agent' to be used for the 
+     * cURL request
+     * 
+     * @return object|boolean An object containing attributes access_token, expires_in,
+     * and refresh_token upon a successful request; returns false upon 
+     * unsuccessful request
+     */
+    public static function refresh_access_token($params) { 
+
+        $required_params = array('client_id', 'client_secret', 'refresh_token');
+        foreach ( $required_params as $param_key ) {
+            if ( !array_key_exists($param_key, $params) ) {
+                throw new MeetupInvalidParametersException($required_params);
+                return FALSE;
+            }
+        }
+
+        # Set up POST parameters to access token endpoint
+        $fields = array(
+            "refresh_token" => $params['refresh_token'],
+            "client_id" => $params['client_id'],
+            "client_secret" => $params['client_secret'],
+            "grant_type" => "refresh_token"
+        );
+
+        $user_agent = NULL; 
+        if ( array_key_exists('user_agent', $params) ) {
+            $user_agent = $params['user_agent'];
+        }
+        
+        $access_response = MeetupOAuth2Helper::_access_request($fields, $user_agent);
+        return $access_response;
+
     }
 }
